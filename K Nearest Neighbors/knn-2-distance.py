b2525cf711ff649921d59cb29eedab600881d89a
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-06-28 20:11:45 -0300

    Dia 28-06

--- K Nearest Neighbors/knn-2-distance.py
+++ K Nearest Neighbors/knn-2-distance.py
@@ -3,12 +3,13 @@ Created on Jul 28, 2018
 Author: @G_Sansigolo
 '''
 from math import sqrt
-
+import numpy as np
 
 plot1 = [1,3]
 plot2 = [2,5]
 
-euclidian_distance = sqrt(  (plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2)
+euclidian_distance = sqrt((plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2)
+#euclidian_distance = np.linalg.norm(np.array(features)-np.array(predict))
 
 print(euclidian_distance)
 
